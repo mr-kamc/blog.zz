19096a973b9e32594dd2034d297762f2e7f3ee62
mr-kamc <mr.kamc@gmail.com>
2016-03-13 23:11:15 +0300

    применены миграции для статей, страниц и категорий

---
+++ database/migrations/2016_03_13_194334_Pages.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Pages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('public');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
---
+++ resources/views/admin/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charaset="utf-8"/>
+    <title>Админка</title>
+    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
+    <script src="{{asset('js/jquery-2.1.4.min.js')}}"></script>
+</head>
+<body>
+<div id="header">
+    <h1>Админка</h1>
+    <div id="content">@yield('content')</div>
+</div>
+</body>
+</html>
